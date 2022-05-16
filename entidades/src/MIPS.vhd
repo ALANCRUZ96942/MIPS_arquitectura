4b9f516c3f29de876aafc25b54ad89b2ff6cead2
ALANCRUZ96942 <alan96942@gmail.com>
2022-05-16 13:29:57 -0500

    in process

--- entidades/src/MIPS.vhd
+++ entidades/src/MIPS.vhd
@@ -8,40 +8,51 @@ entity MIPS is
 	--ELEMENTOS DE MIPS	
 	Generic (size: integer := 32);
 	Port (
-	Address : in  STD_LOGIC_VECTOR(size-1 downto 0);
-	WriteData : in STD_LOGIC_VECTOR (size-1 downto 0);
-	MemoryWrite, MemoryRead, Clock : in STD_LOGIC;
-	ReadData: out STD_LOGIC_VECTOR(size-1 downto 0));
+	InGeneral : in  STD_LOGIC_VECTOR(size-1 downto 0);
+	OutGeneral: out STD_LOGIC_VECTOR(size-1 downto 0)
+	mainClock: in std_logic;
+	);	  
 
 end MIPS;
 
 architecture structural of MIPS is 
 
-	component pc is	
-	    Port ( Input :  in STD_LOGIC_VECTOR (31 downto 0);
-		Output : out STD_LOGIC_VECTOR (31 downto 0);  
-		clock : in STD_logic);		
+	component InstructionMemory is
+		Generic (size: integer := 32);
+    	Port (
+		AddressIM : in  STD_LOGIC_VECTOR(size-1 downto 0);
+		Instruction : out STD_LOGIC_VECTOR (size-1 downto 0);
+		Clock : in STD_LOGIC);
+	end component;	 
+	
+	
+	component MUX1 is
+		 Generic(size:integer := 32);
+	    Port (
+			Data1, Data2: in  STD_LOGIC_VECTOR(size-1 downto 0);
+			Selector: in STD_LOGIC;
+			Result: out STD_LOGIC_VECTOR(size-1 downto 0));
 	end component;
 
-	component ALUS1 is	
-		port (
-		a 			: in std_logic_vector (31 downto 0); --operador1
-		b 			: in std_logic_vector (31 downto 0); --operador2
-		operacion	: in std_logic_vector (3 downto 0); --selector
-		result 		: out std_logic_vector (31 downto 0); --resultado
-		carry_out	: out std_logic;
-		cero 		: out std_logic 
-		);	
+	component Registers is
+	Generic(size:integer := 32);
+    Port (
+		ReadADR1, ReadADR2: in  STD_LOGIC_VECTOR(size-1 downto 0); --Address inputs (5 bits)
+	 	WriteADR1 : IN STd_logic_vector(4 downto 0);
+		WriteReadSEL, clock : in STD_LOGIC; --Control signals: WriteReadSEL	= 0: writing disabled
+		DataIn1: in STD_LOGIC_VECTOR (size-1 downto 0); --Data Input to be saved (32 bits)
+		DataOut1, DataOut2: out STD_LOGIC_VECTOR(size-1 downto 0)); --Data output (32 bits)
 	end component;
 	
+	signal Instruction: std_logic_vector(31 downto 0);
 	
 	
 	
-	
-	
-
-
 begin 
+																									
+	instruction_memory: InstructionMemory port map (AddressIM => InGeneral,Instruction => Instruction, Clock => mainClock);	
+	mux1: MUX1 port map(Data1,Data2,Selector,Result)
+	registers: Registers port map (ReadADR1 => Instruction, ReadADR2 => Instruction, WriteADR1 => ,WriteReadSEL, clock,DataIn1,DataOut1,DataOut2 );	  
 	
-	--ALUS1 port map (a,b	   ..., 
+
 end architecture;
